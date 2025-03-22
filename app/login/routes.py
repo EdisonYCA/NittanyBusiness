84f215a1307d4a05524da3f7bfc6fada248e989d
Edison Caceres Alejo <edisonyca127@gmail.com>
2025-03-22 16:49:20 -0400

    Refactored code structure

--- app/login/routes.py
+++ app/login/routes.py
@@ -1,5 +1,13 @@
 from flask import render_template
 from app.login import bp
+import hashlib
+
+def hashing_password(password):
+    # hashing algorithm to change this password into hashed password
+    sha256 = hashlib.new("SHA256")
+    sha256.update(password.encode())
+    hashed_password = sha256.hexdigest()
+    return hashed_password
 
 @bp.route('/')
 def index():

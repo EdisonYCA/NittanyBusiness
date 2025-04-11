9269c7ed5364cbb3be6f0eaf7a28d8cb3ce6d055
RichardThompson1 <richard.thompson111f3@gmail.com>
2025-04-11 09:31:20 -0400

    Config now has database path information.

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, 'database.db')
+
 class Config:
     FLASK_ENV = 'development'
+    DB_PATH = DB_PATH

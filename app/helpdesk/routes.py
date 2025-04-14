5bafcf89216d72bed483d8623f37764fdec4c1e1
ktruo <kqt5421@psu.edu>
2025-04-14 14:11:40 -0400

    Implemented helpdesk insert request page. No backend or css implemented yet.

--- app/helpdesk/routes.py
+++ app/helpdesk/routes.py
@@ -1,6 +1,6 @@
 from app.helpdesk import bp
 from app import get_db
-from flask import render_template, redirect, request
+from flask import render_template
 import sqlite3
 
 @bp.route('/')
@@ -15,3 +15,16 @@ def index():
     db.close()
 
     return render_template('helpdesk/index.html', result=rows)
+
+@bp.route('/request')
+def request():
+    db = get_db()
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+
+    # status 0 is incomplete, 1 is complete
+    cursor.execute("SELECT * FROM requests WHERE request_status = 0")
+    rows = cursor.fetchall()
+    db.close()
+
+    return render_template('helpdesk/request.html', result=rows)

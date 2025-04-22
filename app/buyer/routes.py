1b99823eb6c86761d6d044fdb8dad8faf71c4b61
ktruo <kqt5421@psu.edu>
2025-04-22 18:28:54 -0400

    Search products by search bar page started. No backend yet to handle query and display page.

--- app/buyer/routes.py
+++ app/buyer/routes.py
@@ -7,3 +7,7 @@ from app.utils.auth import login_required
 @login_required
 def index():
     return render_template('buyer/index.html')
+
+@bp.route('/search', methods=['POST'])
+def search():
+    return render_template('buyer/search.html')

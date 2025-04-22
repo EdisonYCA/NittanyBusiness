df65e1fcf1801101bda21e65fdfa38b4b8f271aa
RichardThompson1 <richard.thompson111f3@gmail.com>
2025-04-22 10:24:28 -0400

    Added redirect to database api, helps avoid redundant database code.

--- app/seller/routes.py
+++ app/seller/routes.py
@@ -1,7 +1,9 @@
 from app.seller import bp
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 
 @bp.route('/')
 def index():
-    return render_template('seller/index.html')
+    return redirect(url_for('api.get_seller_products'))
+
+

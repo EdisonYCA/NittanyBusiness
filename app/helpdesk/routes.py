73ebd58be7e1e048bff1762ad80f06bcb9aa5c91
ktruo <kqt5421@psu.edu>
2025-04-25 14:38:22 -0400

    All functionality of helpdesk page implemented

--- app/helpdesk/routes.py
+++ app/helpdesk/routes.py
@@ -1,6 +1,6 @@
 from app.helpdesk import bp
 from app import get_db
-from flask import render_template
+from flask import render_template, request
 import sqlite3
 
 @bp.route('/')
@@ -45,3 +45,28 @@ def view():
 @bp.route('/addcategory')
 def addcategory():
     return render_template('helpdesk/addcategory.html')
+
+@bp.route('/addcatfunc', methods=['POST'])
+def addcatfunc():
+    cat_name = request.form.get("category_name")
+    parent_name = request.form.get("parent_category_name")
+
+    db = get_db()
+    cursor = db.cursor()
+
+
+
+
+    try:
+        row = cursor.execute("SELECT * FROM Categories WHERE category_name = ?", (cat_name,)).fetchall()
+        if len(row) == 0:
+            cursor.execute("INSERT into Categories (category_name, parent_category) VALUES (?, ?)", (cat_name, parent_name))
+            db.commit()
+        else:
+            return render_template('helpdesk/addcategory.html', ret='fail')
+    except Exception as e:
+        print(f"Database Error: {e}")
+        return f"Error while inserting new category: {e}"
+    finally:
+        db.close()
+    return render_template('helpdesk/addcategory.html', ret='success')

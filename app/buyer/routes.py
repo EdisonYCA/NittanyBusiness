0c5e44770430464b6dd46baf49025ea4e8eeba22
ktruo <kqt5421@psu.edu>
2025-04-27 19:37:45 -0400

    Implemented Setting Page for Buyer

--- app/buyer/routes.py
+++ app/buyer/routes.py
@@ -1,7 +1,9 @@
 from app.buyer import bp
-from flask import render_template, request, make_response, jsonify, session
+from flask import render_template, request, make_response, jsonify, session, redirect, url_for
 from app.utils.auth import login_required
+from app.utils.auth import hash_password
 import sqlite3
+import random
 from app import get_db
 
 
@@ -79,3 +81,95 @@ def get_sub_cat():
     res = make_response(jsonify(result), 200)
 
     return res
+
+@bp.route('get_setting')
+def get_setting():
+    user = session.get('user')
+
+    db = get_db()
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+
+    cursor.execute("""SELECT BUYERS.email, BUYERS.business_name,
+                        CREDIT_CARDS.credit_card_num, CREDIT_CARDS.card_type, CREDIT_CARDS.expire_month, CREDIT_CARDS.expire_year, CREDIT_CARDS.security_code,
+                        ADDRESS.address_id, ADDRESS.zipcode, ADDRESS.street_num, ADDRESS.street_name from Buyers
+                        JOIN USERS on Buyers.email = USERS.email
+                        JOIN Credit_Cards on Buyers.email = Credit_Cards.owner_email
+                        JOIN ADDRESS on Buyers.buyer_address_id = ADDRESS.address_id
+                        WHERE BUYERS.email = ?""", (user,))
+    rows = cursor.fetchall()
+    db.close()
+
+    ret = [dict(row) for row in rows]
+
+    #print(ret)
+    return render_template('buyer/setting.html', result=ret)
+
+@bp.route('update_setting', methods=["POST"])
+def update_setting():
+    user = session.get('user')
+    business_name = request.form.get('business_name')
+    credit_card_num = request.form.get('credit_num')
+    expire_date = request.form.get('expire_date')
+    expire_date_parse = expire_date.split('/')
+    security_code = request.form.get('security_code')
+    zipcode = request.form.get('zip_code')
+    street_name = request.form.get('street_name')
+    street_num = request.form.get('street_num')
+    address_id = request.form.get('address_id')
+    password = request.form.get('password')
+    conf_password = request.form.get('conf_password')
+
+    print(address_id, zipcode, street_name, street_num)
+
+    db = get_db()
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+
+    #cursor.execute("UPDATE Sellers set balance = balance + ? where email = ?", [price, seller_email])
+    cursor.execute("UPDATE BUYERS SET business_name = ? where email = ?",
+                   [business_name, user])
+
+    cursor.execute("UPDATE Credit_Cards SET credit_card_num = ? where owner_email = ?",
+                   [credit_card_num, user])
+    cursor.execute("UPDATE Credit_Cards SET expire_month = ? where owner_email = ?",
+                   [int(expire_date_parse[0]), user])
+    cursor.execute("UPDATE Credit_Cards SET expire_year = ? where owner_email = ?",
+                   [int(expire_date_parse[1]), user])
+    cursor.execute("UPDATE Credit_Cards SET security_code = ? where owner_email = ?",
+                   [security_code, user])
+
+    cursor.execute("UPDATE Address SET zipcode = ? where address_id = ?",
+                   [int(zipcode), address_id])
+    cursor.execute("UPDATE Address SET street_name = ? where address_id = ?",
+                   [street_name, address_id])
+    cursor.execute("UPDATE Address SET street_num = ? where address_id = ?",
+                   [int(street_num), address_id])
+    db.commit()
+
+    if password == conf_password:
+        password = hash_password(password)
+        cursor.execute("UPDATE USERS SET password = ? where email = ?",
+                       [password, user])
+        db.commit()
+        db.close()
+    else:
+        db.close()
+        return redirect(url_for('buyer.get_setting'))
+
+    return redirect(url_for('buyer.get_setting'))
+
+@bp.route('update_email', methods=["POST"])
+def update_email():
+    new_email = request.form.get('new_email')
+    user = session.get('user')
+
+    db = get_db()
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+
+    cursor.execute("INSERT INTO Requests (request_id, sender_email, helpdesk_staff_email, request_type, request_desc, request_status) VALUES (?, ?, ?, ?, ?, ?)",
+                   [random.randint(1, 100000000), user, 'helpdeskteam@nittybiz.com', 'ChangeID', 'Please change my ID to ' + new_email, 0])
+    db.commit()
+    db.close()
+    return redirect(url_for('buyer.get_setting'))
